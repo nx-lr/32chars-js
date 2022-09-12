9097fdc329e96a1615c25a2ef55c1bc641afb359
Frime <90877277+nxltm@users.noreply.github.com>
2022-09-12 12:45:17 +0800

    finally, it works!

--- obfuscator-new.js
+++ obfuscator-new.js
@@ -5,10 +5,12 @@ import fs from 'fs'
 import isValidIdentifier from 'is-valid-identifier'
 import jsesc from 'jsesc'
 import prettier from 'prettier'
-import punycode from 'punycode'
-import type {Lowercase, Uppercase} from './types'
+import punycode, { encode } from 'punycode'
+import type { Lowercase, Uppercase } from './types'
 
 const print = console.log
+
+const text = fs.readFileSync('./test.txt', 'utf8')
 
 // CONSTANTS
 const REGEXES = {
@@ -27,11 +29,10 @@ const REGEXES = {
  *
  * The literal space is ignored since it is the most used
  * character in plain text.
- *
  */
 const REGEX = RegExp(
   _.entries(REGEXES)
-  |> %.map(([key: string, {source}]) => `(?<${key}>${source})`)
+  |> %.map(([key: string, { source }]) => `(?<${key}>${source})`)
   |> %.join`|`
 )
 
@@ -45,7 +46,7 @@ const REGEX = RegExp(
  * The file starts with a _header_ declaration.
  */
 
-function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
+function generateHeader(TEXT, GLOBAL_VAR, { STRICT_MODE = false } = {}) {
   /**
    * Encase a string in literal quotes; finding the shortest
    * ASCII stringified representation of the original string.
@@ -62,12 +63,12 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
       : /'/.test(string)
       ? 'double'
       : 'single'
-    jsesc(string, {quotes: choice, wrap: true})
+    jsesc(string, { quotes: choice, wrap: true })
   }
 
   /**
    * We have 32 characters: `` ;.!:_-,?/'*+#%&^"|~$=<>`@\()[]{} ``
-   * and out of these, subtracting the brackets, we now have
+   * and out of these, subtracting the bracket pairs, we now have
    * 26 characters, enough for the alphabet.
    *
    * So, we would use a cipher of our own, assigning a pair of
@@ -77,6 +78,7 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
    */
   const LETTERS = `abcdefghijklmnopqrstuvwxyz`
   const CIPHER = `;.!:_-,?/'*+#%&^"|~$=<>\`@\\`
+  const SPACE = '-'
 
   const encodeLetter = (char: Lowercase | Uppercase) =>
     (V.isUpperCase(char) ? '$' : '_') +
@@ -95,10 +97,10 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
    */
 
   const CONSTANTS = {
-    true: "!''", // ''==false
+    true: '!""', // ''==false
     false: '![]', // []==true
     undefined: '[][[]]', // [][[]] doesn't exist
-    Infinity: "!''/![]", // true/false==1/0
+    Infinity: '!""/![]', // true/false==1/0
     NaN: '+{}', // It makes sense
     '[object Object]': '{}', // y e s
   }
@@ -121,7 +123,7 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
 
   for (const [constant, expression] of _.entries(CONSTANTS))
     for (const char of constant)
-      if (/\w/.test(char) && !(char in CHARSET_1))
+      if (/[\w\s]/.test(char) && !(char in CHARSET_1))
         CHARSET_1[char] = [expression, constant.indexOf(char)]
 
   const RES_CHARSET_1 =
@@ -138,6 +140,7 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
     |> %.flat().join`,`
     |> %.replace(/,$/, '').replace(/,+/g, ',')
     |> [GLOBAL_VAR + '={' + % + '}'][0]
+    |> %.replace('_' + void 0, SPACE) // Replace space
 
   RESULT += RES_CHARSET_1
 
@@ -181,14 +184,14 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
     Function: '(()=>{})',
   }
 
-  const SIGILS = {...IDENT_SET1, ...LITERALS, space: '_'}
-  const CHARSET_2 = {...CHARSET_1}
+  const SIGILS = { ...IDENT_SET1, ...LITERALS, space: '_' }
+  const CHARSET_2 = { ...CHARSET_1 }
 
   for (const [key, expression] of _.entries(LITERALS)) {
     let index
     const constructor = eval(key).toString()
     for (const char of constructor)
-      if (/\w/.test(char) && !(char in CHARSET_2))
+      if (/[\w\s]/.test(char) && !(char in CHARSET_2))
         CHARSET_2[char] = [expression, (index = constructor.indexOf(char))]
   }
 
@@ -209,7 +212,7 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
     if (!(char in CHARSET_1)) CHARSET_2[char] = [expression, index, expansion]
   }
 
-  const objectDifference = (x: {[val]: any}, y: {[val]: any}) =>
+  const objectDifference = (x: object, y: object) =>
     _.fromPairs(_.difference(_.keys(x), _.keys(y)).map(z => [z, x[z]]))
   const CHARSET_2_DIFF = objectDifference(CHARSET_2, CHARSET_1)
 
@@ -249,18 +252,20 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
 
   // String encoding
   const encodeString = (str: string = ''): string =>
-    [...str.replace(/\W/g, '')].map(
+    [...('' + str).replace(/\W/g, '')].map(
       char => do {
-        const encoded = encodeLetter(char)
-        ;/[$_]/.test(char)
-          ? quote(char)
-          : isValidIdentifier(encoded)
-          ? GLOBAL_VAR + '.' + encoded
-          : GLOBAL_VAR + '[' + quote(encoded) + ']'
+        if (/[$_]/.test(char)) quote(char)
+        else if (/\d/.test(char)) GLOBAL_VAR + '.' + encodeDigit(char)
+        else {
+          const encoded = encodeLetter(char)
+          isValidIdentifier(encoded)
+            ? GLOBAL_VAR + '.' + encoded
+            : GLOBAL_VAR + '[' + quote(encoded) + ']'
+        }
       }
     ).join`+`
 
-  const encodeIdentifiers = (identifiers: {[ident]: string}): string =>
+  const encodeIdentifiers = (identifiers: { [ident]: string }): string =>
     _.entries(identifiers)
     |> %.map(([ident, key]) => [key, encodeString(ident)])
     |> %.map(
@@ -281,11 +286,11 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
     map: '^',
     replace: ':',
     repeat: '*',
+    split: '|',
+    indexOf: '#',
   }
 
   RESULT += ';' + encodeIdentifiers(IDENT_SET2)
-
-  /*DEBUG*/ RESULT += ';console.log(' + GLOBAL_VAR + ')'
 
   /**
    * STEP 3: FUNCTIONS
@@ -300,7 +305,16 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
    *
    * The `escape` function would help us retrieve the characters C and D
    * from the code points of our palette of ASCII symbols.
-   *
+   */
+
+  const GLOBAL_FUNC = {
+    eval: '=',
+    escape: '>',
+    unescape: '<',
+    parseInt: '+',
+  }
+
+  /**
    * We would need to get the method `toString` by getting the `name`
    * of the `String` constructor, for us to retrieve the rest of the
    * alphabet, and use to retrieve words from 64-bit float numbers.
@@ -318,14 +332,208 @@ function generateHeader(GLOBAL_VAR = '$', {STRICT_MODE = false} = {}) {
    * future use.
    */
 
-  const FUNCTIONS_SET1 = {
-    eval: '=',
-    escape: '>',
-    unescape: '<',
-    parseInt: '~',
+  const RES_FUNCTIONS_1 = _.entries(GLOBAL_FUNC).map(
+    ([ident, shortcut]) => do {
+      GLOBAL_VAR +
+        '[' +
+        quote(shortcut) +
+        ']=' +
+        LITERALS.Function +
+        '[' +
+        [GLOBAL_VAR + '.$'][0] + // `constructor`
+        '](' +
+        [GLOBAL_VAR + '._'][0] + // `return`
+        '+' +
+        [GLOBAL_VAR + '[' + quote(SPACE) + ']'][0] + // space
+        '+' +
+        encodeString(ident) + // name of function
+        ')()'
+    }
+  ).join`;`
+
+  RESULT += ';' + RES_FUNCTIONS_1
+
+  // toString
+  const TO_STRING = "'"
+  RESULT +=
+    ';' +
+    GLOBAL_VAR +
+    '[' +
+    quote(TO_STRING) +
+    ']=' +
+    encodeString('to') +
+    '+' + // no word breaks
+    LITERALS.String +
+    // `constructor`
+    ['[' + GLOBAL_VAR + '.$' + ']'][0] +
+    // `name`
+    ['[' + GLOBAL_VAR + '[' + quote(IDENT_SET2.name) + ']]'][0]
+
+  /**
+   * We would use the escape function to get the letters C and D
+   * by escaping it as a URL and then getting the last digit of
+   * the codepoint which is always uppercase.
+   *
+   * All invalid URL characters are immediately converted to the
+   * format `%XX` where `X` is a hexadecimal digit in uppercase.
+   *
+   * Since `-` is a valid URL character, `=` will be used instead
+   * since it has semantic meaning in URLs (key-value pairs).
+   *
+   * @example
+   * $.C = escape(',')[2]
+   * $.D = escape(',')[2]
+   */
+
+  RESULT +=
+    ';' +
+    GLOBAL_VAR +
+    '[' +
+    quote(encodeLetter('C')) +
+    ']=' +
+    GLOBAL_VAR +
+    '[' +
+    quote(GLOBAL_FUNC.escape) +
+    '](' +
+    quote('<') + // U+3C
+    ')[' +
+    GLOBAL_VAR +
+    '.' +
+    encodeDigit(2) +
+    ']'
+
+  RESULT +=
+    ';' +
+    GLOBAL_VAR +
+    '[' +
+    quote(encodeLetter('D')) +
+    ']=' +
+    GLOBAL_VAR +
+    '[' +
+    quote(GLOBAL_FUNC.escape) +
+    '](' +
+    quote('=') + // U+3D
+    ')[' +
+    GLOBAL_VAR +
+    '.' +
+    encodeDigit(2) +
+    ']'
+
+  /**
+   * U is retrieved from the following formula:
+   *
+   * @example
+   * Object.toString.call().toString()
+   * @end
+   *
+   * We would use `U` and `C` to form the method name `toUpperCase`,
+   * to retrieve the remaining uppercase letters.
+   */
+
+  RESULT +=
+    ';' +
+    GLOBAL_VAR +
+    '[' +
+    quote(encodeLetter('U')) +
+    ']=' +
+    '`${' +
+    LITERALS.Object +
+    ['[' + GLOBAL_VAR + '[' + quote(TO_STRING) + ']]'][0] +
+    ['[' + GLOBAL_VAR + '[' + quote(IDENT_SET1.call) + ']]'][0] +
+    '()}`[' +
+    GLOBAL_VAR +
+    '.' +
+    encodeDigit(8) +
+    ']'
+
+  /**
+   * We will get thr remainder of the ASCII alphabet so we can
+   * splice it up with some regex.
+   */
+
+  const CIPHER_FROM =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  // Remaining characters
+  const CHARSET_3 = [..._.keys(CHARSET_2), ...'CDU']
+    .filter(char => !!char.trim() || V.isNumeric(char))
+    .sort()
+  const REMAINING_CHARS = _.difference([...CIPHER_FROM], CHARSET_3)
+
+  console.log(CHARSET_3)
+
+  for (const letter of 'hkqwz')
+    RESULT +=
+      ';' +
+      GLOBAL_VAR +
+      '[' +
+      quote(encodeLetter(letter)) +
+      ']=' +
+      '(+(' +
+      encodeString(CIPHER_FROM.indexOf(letter)) +
+      '))[' +
+      GLOBAL_VAR +
+      '[' +
+      quote(TO_STRING) +
+      ']](' +
+      encodeString(36) +
+      ')'
+
+  const IDENT_SET3 = { fromCharCode: '@' }
+  const CIPHER_TO = `_.:;!?*+^-=<>~'"/|#$%&@{}()[]\`\\`
+
+  const base31toUtf16 = (b: string) =>
+    String.fromCharCode(
+      ...b.split`,`.map(s =>
+        parseInt([...s].map(c => cipherFrom[cipherTo.indexOf(c)]).join``, 31)
+      )
+    )
+
+  RESULT += ';' + encodeIdentifiers(IDENT_SET3)
+
+  const IDENT_SET = {
+    ...IDENT_SET1,
+    ...IDENT_SET2,
+    ...IDENT_SET3,
   }
+
+  const CONSTANT_CIPHER =
+    GLOBAL_VAR + '[' + encodeString(0) + ']=' + encodeString(CIPHER_FROM)
+
+  const MACRO_B31_UTF16 =
+    'a=(a=>a.split`,`.map(a=>parseInt([...a].map(a=>CIPHER_FROM[CIPHER_TO.indexOf(a)]).join``,+(31))).map(a=>String.fromCharCode(a)).join``)'
+      .replace(/^\w=|;$/g, '')
+      .replace('CIPHER_TO', quote(CIPHER_TO))
+      .replace('String', LITERALS.String + '[' + GLOBAL_VAR + '.$]')
+      .replace(/\d+/, match => encodeString(match))
+      .replace('parseInt', GLOBAL_VAR + '[' + quote(GLOBAL_FUNC.parseInt) + ']')
+      .replace(/\ba\b/g, '_' + GLOBAL_VAR)
+      .replace(
+        /\.(?<ident>split|map|indexOf|join|fromCharCode)\b/g,
+        ident => do {
+          ident = ident.replace(/^\./, '')
+          '[' + GLOBAL_VAR + '[' + quote(IDENT_SET[ident]) + ']]'
+        }
+      )
+      .replace('CIPHER_FROM', GLOBAL_VAR + '[+![]]')
+      .replace(/^/, GLOBAL_VAR + '[+!""]=')
+
+  const utf16toBase31 = (s: string) =>
+    `${[...Array(s.length)].map(
+      (x, i) =>
+        [...s.charCodeAt(i).toString(31)].map(
+          c => CIPHER_TO[CIPHER_FROM.indexOf(c)]
+        ).join``
+    )}`
+
+  RESULT += ';' + CONSTANT_CIPHER
+  RESULT += ';' + MACRO_B31_UTF16
+
+  /** For debugging purposes only. */
+  RESULT +=
+    ';console.log(' + GLOBAL_VAR + "[+!''](" + quote(utf16toBase31(TEXT)) + '))'
 
   return RESULT
 }
 
-print(generateHeader('_'))
+fs.writeFileSync('./run.js', generateHeader(text, '_', { STRICT_MODE: true }))
