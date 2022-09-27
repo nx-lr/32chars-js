5aa176dc9656b92f9d98d70c223e5148aa906e87
Frime <90877277+nxltm@users.noreply.github.com>
2022-09-27 22:36:32 +0800

    Improved testing

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
+require("colors");
+const Diff = require("diff");
 const {result} = require("./output");
 
+console.log(`Running test...`);
+
 const text = fs.readFileSync("./input.txt", "utf8");
-console.log(`Compared: ${text === result}`);
+console.log(`Compared: ${text == result}.`);
+
+const diff = Diff.diffChars(text, result);
+diff.forEach(part => {
+  const color = part.added ? "green" : part.removed ? "red" : "grey";
+  process.stderr.write(part.value[color]);
+});
+
+console.log(`Writing evaluated output...`);
 fs.writeFileSync("./result.txt", result);
